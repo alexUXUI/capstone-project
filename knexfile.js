e34c8272fecd03073b895db04e09724f4d989cd2
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-12 13:35:46 -0700

    config :wrench:

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection:  'postgres://yyievtixfwtnlx:fL8_qZmV4x0XG1OjZAEnauPdhp@ec2-54-227-250-148.compute-1.amazonaws.com:5432/dd4mcmurpta322?ssl=true'
+
+  }
+
+};
