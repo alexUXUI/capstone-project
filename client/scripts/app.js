fb1d6371deb651e253ac94735c415ba50e478d6e
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-12 13:39:32 -0700

    routing :vertical_traffic_light:

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 angular.module('capstone', ['ui.router'])
   .config(function($stateProvider, $urlRouterProvider){
-      $urlRouterProvider.otherwise('/');
+      $urlRouterProvider.otherwise('/landing');
       $stateProvider.state('onboard', {
         templateUrl: 'templates/onboard.html',
         controller: 'LandingController',
