21a9920c72d5fd89c3eccd0fff62e7f0cc352948
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-12 14:08:54 -0700

    taking out bugs

--- client/scripts/controllers.js
+++ client/scripts/controllers.js
@@ -11,19 +11,19 @@ function MainController($scope, $http) {
 
   $scope.whatever = 'whatever';
 
-  $scope.getUsers = $http({
-  method: 'GET',
-  url: '/mainpage'
-}).then(function successCallback(response) {
-  var users = response;
-  
-  console.log(response);
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response) {
-    console.log('something went wrong');
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+//   $scope.getUsers = $http({
+//   method: 'GET',
+//   url: '/mainpage'
+// }).then(function successCallback(response) {
+//   var users = response;
+//
+//   console.log(response);
+//     // this callback will be called asynchronously
+//     // when the response is available
+//   }, function errorCallback(response) {
+//     console.log('something went wrong');
+//     // called asynchronously if an error occurs
+//     // or server returns response with an error status.
+//   });
 
 }
