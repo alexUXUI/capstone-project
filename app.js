6294ea60539ea08680bb99aae12c2450378fe64f
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-12 14:06:03 -0700

    adding C(R)UD

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
@@ -13,6 +14,8 @@ var routes = require('./routes/index');
 var auth = require('./routes/auth');
 var users = require('./routes/users');
 var app = express();
+var db = require('knex')
+var knex = require('./db/knex');
 ////////////////////////////////////////////////////////////////////////////////
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -57,6 +61,16 @@ app.get('/logout', function(req, res, next){
   req.logout();
   res.redirect('/')
 })
+////////////////////////////////////////////////////////////////////////////////
+app.get('/mainpage', function(req, res, next){
+  knex('users').select().then(function(data){
+    console.log('sending back data');
+    res.status(200).json({data: data})
+  })
+})
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 // catch 404 and forward to error handler
